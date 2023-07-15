2adb1ac4bfa648f6fb4492d48f4c21275bb533eb
Юлиана Стрельникова <julianka.str@gmail.com>
2023-07-15 20:32:00 +0300

    debug

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '1234')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.30.4', '127.0.0.1', 'localhost', 'my-kittygram.zapto.org']
 
